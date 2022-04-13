5c5a135808302deff30f3d51a1fd35569d1eadea
JDBIRLA <69948118+jdbirla@users.noreply.github.com>
2022-04-13 14:01:03 +0900

    JD_IMP_SQL Oracle sql developer guide

---
+++ ORACLE_SQL_DEVLOPER_GUIDE.sql
@@ -0,0 +1,14 @@
+--1. Change language :  add entry in sqldeveloper.conf => AddVMOption -Duser.language=en
+--2. Disabling some feature for improve performance :  ==> Tools -> Features -> disable which you are not working
+--3. Null with backgroun colir :  Tools -> preference -> database -> advance
+--4. We can rename Query result : Right click and give name
+--5. Copy data with column hearder  :  ctrl + shift + c
+--6. Code template : code eidtor -> code template  
+--7. Code snippets : view -> snippets (useful when you don't remeber the code syntax , just drag and drop to worksheet) 
+--8. Query Result in different color for each row : Tools ->preference -> database -> worksheet -> grid in checker bord
+--9. short cut for table grid: Shift + F4
+--10. Gauge chart : 'SQLDEV:GAUGE:<min>:<max>:<low-threshold>:<high-threshold>:<gauge-value>'
+
+
+
+
